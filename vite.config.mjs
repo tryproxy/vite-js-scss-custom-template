cdb6c3baefe55590a92ff4ded389af2f7603a99e
asdqdsa <asdqdsa@protonmail.com>
2025-11-29 02:51:39 +0400

    refactor: change global template vars for dev/prod HTML, remove .env usage

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,46 +11,61 @@ import _config from './_config.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig({
-  root: _config.root,
-  server: {
-    host: _config.server.host,
-    port: _config.server.port,
-    open: _config.server.open,
-    watch: {
-      ignored: ['**/.stylelintcache', '**/.eslintcache'],
-    },
-  },
-  preview: { port: _config.preview.port },
-  plugins: [
-    removeConsole({}),
-    eslint({ cache: true }),
-    stylelint(),
-    legacy({ targets: _config.legacyTargets }),
-  ],
-  build: {
-    sourcemap: true,
-    minify: false,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, _config.root, 'index.html'),
-        atomic: resolve(
-          __dirname,
-          _config.srcPath,
-          'shared/uikit/atomic/bundle.js'
-        ),
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  const APP_TITLE = isProd ? 'App' : 'DEVENV';
+  const FAVICON = isProd ? '/favicon-prod.svg' : '/favicon-dev.svg';
+
+  return {
+    define: {},
+    root: _config.root,
+    server: {
+      host: _config.server.host,
+      port: _config.server.port,
+      open: _config.server.open,
+      watch: {
+        ignored: ['**/.stylelintcache', '**/.eslintcache'],
       },
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name?.includes('atomic')) return 'assets/atomic.css';
-          return 'assets/[name]-[hash][extname]';
+    },
+    preview: { port: _config.preview.port },
+    plugins: [
+      removeConsole({}),
+      eslint({ cache: true }),
+      stylelint(),
+      legacy({ targets: _config.legacyTargets }),
+      {
+        name: 'inject-html-vars',
+        transformIndexHtml(html) {
+          return html
+            .replace(/%__APP_TITLE__%/g, APP_TITLE)
+            .replace(/%__FAVICON__%/g, FAVICON);
+        },
+      },
+    ],
+    build: {
+      sourcemap: true,
+      minify: false,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, _config.root, 'index.html'),
+          atomic: resolve(
+            __dirname,
+            _config.srcPath,
+            'shared/uikit/atomic/bundle.js'
+          ),
+        },
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name?.includes('atomic')) return 'assets/atomic.css';
+            return 'assets/[name]-[hash][extname]';
+          },
         },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, _config.srcPath),
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, _config.srcPath),
+      },
     },
-  },
+  };
 });
