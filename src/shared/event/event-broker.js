6a79c917aa2a08d206615f121bb03464fc56fd7d
asdqdsa <asdqdsa@protonmail.com>
2025-11-29 04:43:40 +0400

    add CustomEvent-based event broker utility

--- src/shared/event/event-broker.js
+++ src/shared/event/event-broker.js
@@ -1,13 +1,33 @@
+/**
+ * Event broker with CustomEvent
+ * @namespace events
+ */
 export const events = {
-  emit(event, detail) {
+  on: (event, cb) => document.addEventListener(event, cb),
+
+  off: (event, cb) => document.removeEventListener(event, cb),
+
+  emit: (event, detail = null) => {
     document.dispatchEvent(new CustomEvent(event, { detail }));
+    document.dispatchEvent(new CustomEvent('*', { detail: { event, detail } }));
   },
 
-  on(event, cb) {
-    document.addEventListener(event, cb);
-  },
+  /**
+   * Catches all '*' events
+   * @param {(payload: { event: string; detail: unknown }) => void} cb
+   */
+  all: (cb) =>
+    document.addEventListener('*', (e) => {
+      const customEvent =
+        /** @type {CustomEvent<{ event: string; detail: unknown }>} */ (e);
+      cb(customEvent.detail);
+    }),
 
-  off(event, cb) {
-    document.removeEventListener(event, cb);
+  once: (event, cb) => {
+    const handleOnce = (e) => {
+      cb(e);
+      document.removeEventListener(event, handleOnce);
+    };
+    document.addEventListener(event, handleOnce);
   },
 };
