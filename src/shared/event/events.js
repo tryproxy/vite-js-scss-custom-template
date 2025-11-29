b27e2e384b47618e93050bb1132c19c99977b96f
asdqdsa <asdqdsa@protonmail.com>
2025-11-29 04:45:50 +0400

    add example events

---
+++ src/shared/event/events.js
@@ -0,0 +1,27 @@
+// CONST EVENTS
+
+export const APP_EVENTS = {
+  UI_MENU_ACTION: 'ui:menu-action',
+
+  UI_MODE_SELECT: 'ui:mode-select',
+  UI_GAME_SETTINGS: 'ui:game-settings',
+  UI_GAME_STATS: 'ui:game-stats',
+  UI_BACK_TO_MENU: 'ui:back-to-menu',
+
+  UI_GAME_ACTION: 'ui:game-action',
+
+  GAME_START: 'game:start',
+  GAME_END: 'game:end',
+  GAME_CONTINUE: 'game:continue',
+  GAME_UPDATED: 'game:updated',
+  GAME_TICK: 'game:tick',
+
+  STORAGE_SAVE: 'storage:save',
+  STORAGE_LOAD: 'storage:load',
+
+  THEME_NEXT: 'theme:next',
+  THEME_UPDATED: 'theme:updated',
+
+  LANG_NEXT: 'lang:next',
+  LANG_UPDATED: 'lang:updated',
+};
